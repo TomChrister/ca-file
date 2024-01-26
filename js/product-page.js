cfa23fac0e1a989c22c083189b96251d070a423f
TOMCHRISTER\tomcs <tomchrister.s@outlook.com>
2024-01-26 14:06:34 +0100

    no message

--- js/product-page.js
+++ js/product-page.js
@@ -1,23 +1,44 @@
+// First button
 document.getElementById('openModalBtn').addEventListener('click', function() {
-    document.getElementById('modal').style.display = 'block';
-    document.getElementById('modalContent').style.display = 'block';
-    document.body.style.overflow = 'hidden';
+    openModal('modal', 'modalContent');
 });
 
+// Second button
+document.getElementById('openModalBtn2').addEventListener('click', function() {
+    openModal('modal2', 'modalContent2');
+});
+
+function openModal(modalId, contentId) {
+    document.getElementById(modalId).style.display = 'block';
+    document.getElementById(contentId).style.display = 'block';
+    document.body.style.overflow = 'hidden';
+}
+
 document.getElementById('closeModalBtn').addEventListener('click', function() {
-    closeModal();
+    closeModal('modal', 'modalContent');
+});
+
+document.getElementById('closeModalBtn2').addEventListener('click', function() {
+    closeModal('modal2', 'modalContent2');
 });
 
 document.getElementById('modal').addEventListener('click', function(event) {
     // Close the modal only if the overlay (outside the modal content) is clicked
     if (event.target === document.getElementById('modal')) {
-        closeModal();
+        closeModal('modal', 'modalContent');
     }
 });
 
-function closeModal() {
-    document.getElementById('modal').style.display = 'none';
-    document.getElementById('modalContent').style.display = 'none';
+document.getElementById('modal2').addEventListener('click', function(event) {
+    // Close the modal only if the overlay (outside the modal content) is clicked
+    if (event.target === document.getElementById('modal2')) {
+        closeModal('modal2', 'modalContent2');
+    }
+});
+
+function closeModal(modalId, contentId) {
+    document.getElementById(modalId).style.display = 'none';
+    document.getElementById(contentId).style.display = 'none';
     document.body.style.overflow = 'auto';
 }
 
